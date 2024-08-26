4b7c2210af33d0490f8123b276130f612676f67e
turtlelazy <ishraqmahid@gmail.com>
2024-08-26 04:26:18 -0400

    conf change

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: "/",
+    images: {
+        unoptimized: true
+    },
     output: "export",  // <=== enables static exports
 
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  images: {
+    unoptimized: true
+  },
+
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
